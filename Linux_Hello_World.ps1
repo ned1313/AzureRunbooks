0ca9a041abe98e77df754e91ba5a4843071a9407
Ned Bellavance <ned1313@users.noreply.github.com>
2016-01-06 16:05:30 -0500

    Updated Linux_Hello_World.ps1 in Azure Automation.

--- Linux_Hello_World.ps1
+++ Linux_Hello_World.ps1
@@ -1,8 +1,18 @@
+
 workflow Linux_Hello_World
 {
+	param(
+		[parameter(Mandatory)]
+		[String]$username,
+		[parameter(Mandatory)]
+		[String]$password
+)
+	$pass = ConvertTo-SecureString `
+		-AsPlainText $password -Force
+	$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username,$pass
 	Invoke-SSHCommand `
 		-ComputerName "MARSMaster01.cloudapp.net" `
-		-Credential (Get-Credential) `
+		-Credential $cred `
 		-Port 22 `
 		-ScriptBlock {
 			sudo apt-get update
